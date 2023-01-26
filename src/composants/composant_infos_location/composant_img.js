766960e1386a6c6fd8df53d8cefd09d8fd0ce7ea
CurtCorentin <108574301+CurtCorentin@users.noreply.github.com>
2023-01-26 19:28:50 +0100

    sd

--- src/composants/composant_infos_location/composant_img.js
+++ src/composants/composant_infos_location/composant_img.js
@@ -1,62 +1,48 @@
 import React from 'react';
-import locations from '../../dataBase/Locations.json';
+
 import IdLocation from './fonctions_infos_location/recuperationId';
 import { IoIosArrowBack,IoIosArrowForward } from "react-icons/io";
+import { useState } from 'react';
 
 const Composant_img = () => {
 
 const loc = IdLocation([0]);////infos location
+const [current, setCurrent] = useState(0); //je définie l'index du premier slide à 0
+const length = loc.pictures.length; // longueur du tableau de slides
 
-let Url = window.location.toString().split("/");
-let idUrl = Url[Url.length-1]
-
-
-let idLocations = []
-var indexLocation = 0
-for (let i in locations)idLocations.push(locations[i].id);
-for (let i in idLocations )if(idLocations[i] === idUrl)indexLocation = i;
-
-const majUrl = () => {
-Url.pop()
-Url.push(idUrl)
-window.location = Url.join("/")
-console.log(idUrl,"idUrl",indexLocation,"indexLocation");
-}
-
-const indexDown = () =>{
---indexLocation;
-
-if(indexLocation < 0 ){
-indexLocation = idLocations.length-1;
-idUrl = idLocations[indexLocation]
-majUrl()
-
-}else {
-idUrl = idLocations[indexLocation];
-majUrl()
-}
-}
-
-const indexUp = () =>{
-++indexLocation
-
-if(indexLocation >= idLocations.length ){
-indexLocation =0
-idUrl = idLocations[indexLocation];
-majUrl()
-
-}else {
-idUrl = idLocations[indexLocation];
-majUrl()
-}
-}
+const nextSlide = () => {
+   setCurrent(current === length - 1 ? 0 : current + 1); // on repart au premier slide quand on arrive au dernier
+};
+const prevSlide = () => {
+   setCurrent(current === 0 ? length - 1 : current - 1); // on repart au dernier slide quand on est au premier
+};
 
 return (
-<div className="divimgInfo">
-<button className="carrousselDown" onClick={indexDown}><IoIosArrowBack/></button>
-<img className="imgInfo" src={loc.cover} alt={loc.title} /> 
-<button className="carrousselUp" onClick={indexUp}><IoIosArrowForward/></button>
-</div>
+   <section id="carrousel-container">
+      {length > 1 && (
+    <div className='carrousselDown'>{<IoIosArrowBack onClick={prevSlide}/>}</div>
+      )}
+      {length > 1 && (
+       <div className='carrousselUp'>{<IoIosArrowForward onClick={nextSlide}/>}</div>
+      )}
+      {loc.pictures.map((slide, index) => (
+         <div
+            key={index} // mise en place du slider avec affichage conditionnel et opacity=1 quand le slide en cours vaut l'index
+            className={
+               current === index
+                  ? "slider bl-msk wh-msk active-anim"
+                  : "slider bl-msk wh-msk"
+            }
+         >
+            {index === current && <img className='imgInfo' src={slide} alt="appartement à louer" />}
+            {index === current && (
+               <span className="slider__number">
+                  {current + 1}/{length}
+               </span>
+            )}
+         </div>
+      ))}
+   </section>
 );
 };
 
