b2494edd93fdfcb51913014d6c615c6f5b340a17
CurtCorentin <108574301+CurtCorentin@users.noreply.github.com>
2023-01-30 07:10:52 +0100

    qq

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import Home from './pages/Home';
-import InfosLocation from './pages/InfoLocations';
-import Apropos from './pages/Apropos';
-import UrlNotFound from './pages/UrlNotFound';
-
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Header from "./components/Header/Header";
+import FicheLogement from "./pages/FicheLogement/FicheLogement";
+import Footer from "./components/Footer/Footer";
+import Error from "./pages/Error/Error.jsx";
 
 function App() {
-  return (
-    <>
-    <BrowserRouter>
-    
-     <Routes>
-       <Route path="/"exact element={<Home/>} />
-       <Route path="/InfoLocation/:id" element={<InfosLocation/>} />
-       <Route path="/Apropos" element={<Apropos/>} />
-       <Route path="*" element={<UrlNotFound/>} />
-       </Routes>
-     
-     </BrowserRouter>
- </> );
+	return (
+		<div className="App">
+			<Header />
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/logement/:id" element={<FicheLogement />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
